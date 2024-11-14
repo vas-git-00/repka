20e8700cab3a7364c2493d53181a7c2e6be82eaa
vas-git-00 <vasyukov.a.o@gmail.com>
2024-11-14 13:37:24 +0300

    upd: config uri

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,4 +1,5 @@
-export const URI_BACKEND = window.location.hostname === 'localhost'
-        
+const isLocalhost = ['localhost', '127.0.0.1'].includes(window.location.hostname)
+
+export const URI_BACKEND = isLocalhost
         ? 'http://localhost:8800'
         : 'http://194.87.214.60:8800'
