51d8b8d731ed721d1162e5d64ce7792cdf68751d
vas-git-00 <vasyukov.a.o@gmail.com>
2024-11-14 14:29:06 +0300

    fix: vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-    
-  
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["legacy-js-api"],
+        additionalData: `
+          @import "src/styles/variables.scss";
+          @import "src/styles/global.scss";
+          @import "src/styles/responsive.scss";
+        `
+      },
+    },
+  },
 })
-
-
-//css: {
-//  preprocessorOptions: {
-//    scss: {
-//      silenceDeprecations: ["legacy-js-api"],
-//    },
