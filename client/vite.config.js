6598fe080855e143da4a3d7e4266101aef8a6206
vas-git-00 <vasyukov.a.o@gmail.com>
2024-11-14 15:55:23 +0300

    fix: scss

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+//import vitePluginSass from 'vite-plugin-sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 
   plugins: [react()],
   
-  /*resolve: {
-    alias: {
-      'sass': 'sass-embedded',
-    },
-  },*/
-  
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler',
-        //silenceDeprecations: ["legacy-js-api"], test commit test commit
+        //api: 'modern-compiler',
+        silenceDeprecations: ["legacy-js-api"], //test commit test commit
       },
     },
   },
+  
 })
+
